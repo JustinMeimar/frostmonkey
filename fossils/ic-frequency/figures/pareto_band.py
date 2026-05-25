608815dcde82bb6cf0e9209d90d456c5f255443e
justinmeimar <meimar@ualberta.ca>
2026-05-25 15:27:42 -0600

    sync fossils

--- fossils/ic-frequency/figures/pareto_band.py
+++ fossils/ic-frequency/figures/pareto_band.py
@@ -1,17 +1,15 @@
 #!/home/justin/tools/fossil/figures/.venv/bin/python
 import sys
-from fossil_figures import apply_style, load_stdin, get_colors, ranked_cdf
+from fossil_figures import apply_style, load_stdin, ranked_cdf_band
 
-apply_style(column="double")
+apply_style(column="single")
 data = load_stdin()
-colors = get_colors(data.column_names)
-fig = ranked_cdf(
+fig = ranked_cdf_band(
     data,
     metric="ranked_counts",
     xlabel="Unique stub bodies (ranked by frequency)",
     ylabel="Fraction of total attaches",
     thresholds=[0.9],
     log_x=True,
-    colors=colors,
 )
 fig.savefig(sys.argv[1])
